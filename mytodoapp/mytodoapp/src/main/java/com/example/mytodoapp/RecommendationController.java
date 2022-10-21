d9f0fb906fbaf2abb8da9091216258bee814e3d0
inmyownway <wjdals05070@gmail.com>
2022-10-21 13:46:23 +0900

    todoapp

---
+++ mytodoapp/mytodoapp/src/main/java/com/example/mytodoapp/RecommendationController.java
@@ -0,0 +1,14 @@
+package com.example.mytodoapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationController {
+
+    @GetMapping("/")
+    public String helloWorld()
+    {
+        return "To-do-Application";
+    }
+}
